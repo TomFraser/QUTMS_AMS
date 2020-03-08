a9561a86d08dc37650a5daf69be7d9d3e8de9f54
Smuggler@Fury <mattsadov@gmail.com>
2020-03-08 21:41:11 +1000

    Converted code from .ino to .c

---
+++ AMS_Test/AMS_Test.c
@@ -0,0 +1,73 @@
+#include "AMS_Test.h"
+
+uint8_t sdFault = 0;
+uint8_t bmsFault = 0;
+
+uint8_t counter = 1;
+uint8_t readCounter = 0;
+
+char buf[48];
+
+void board_init(void) {
+    // Set Relay control pins as outputs to control triggers
+    SET_OUTPUT(RELAY_PORT, RELAY_CONTROL_1);
+    SET_OUTPUT(RELAY_PORT, RELAY_CONTROL_2);
+    SET_OUTPUT(RELAY_PORT, RELAY_CONTROL_3);
+    SET_OUTPUT(RELAY_PORT, RELAY_CONTROL_4);
+
+    // DDR_RELAY = (1<<RELAY_CONTROL_1)|(1<<RELAY_CONTROL_1)|
+    //             (1<<RELAY_CONTROL_1)|(1<<RELAY_CONTROL_1);
+
+    // Use pins to inform about failure from each side
+    SET_INPUT(DDR_FAULT, BMS_FAULT_PIN);
+    SET_INPUT(DDR_FAULT, SHUTDOWN_PIN);
+
+    // Set HIGH to disable relay triggers
+    WRITE_BIT(RELAY_PORT, RELAY_CONTROL_1, HIGH);
+    WRITE_BIT(RELAY_PORT, RELAY_CONTROL_2, HIGH);
+    WRITE_BIT(RELAY_PORT, RELAY_CONTROL_3, HIGH);
+    WRITE_BIT(RELAY_PORT, RELAY_CONTROL_4, HIGH);
+}
+
+void board_routine(void) {
+
+    // TODO: Debouncing may not be necessary in this setup
+
+    snprintf(buf, sizeof(buf), "ShutDown: ( %d ) ||| BMSFault: ( %d )\r\n", sdFault, bmsFault);
+    send_buffer( buf );
+
+    // if we're not getting anything from the shutdown line turn everything off
+    if(BIT_IS_SET(FAULT_PORT, SHUTDOWN_PIN)) {
+        sdFault = 1;
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_1, LOW);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_2, HIGH);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_3, HIGH);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_4, HIGH);
+    }
+    if(BIT_IS_SET(FAULT_PORT, BMS_FAULT_PIN))
+        bmsFault = 1;
+
+    if (readCounter < 20) {
+        readCounter++;
+        _delay_us(10);
+        return;
+    }
+    
+    if(bmsFault == 0 && sdFault == 0) {             // No issues
+        // only run this on start up
+        // but after we've checked pins are fine
+        if(counter == 1) {
+            _delay_ms(100);
+            WRITE_BIT(RELAY_PORT, RELAY_CONTROL_4, LOW);
+            _delay_ms(1000);
+            WRITE_BIT(RELAY_PORT, RELAY_CONTROL_3, LOW);
+            _delay_ms(100);
+            counter = 0;
+        }        
+    } else if (bmsFault == 1 || sdFault == 1) {     // Failure
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_1, LOW);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_2, HIGH);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_3, HIGH);
+        WRITE_BIT(RELAY_PORT, RELAY_CONTROL_4, HIGH);
+    }
+}
---
+++ Makefile
@@ -0,0 +1,21 @@
+TARGETS=main
+
+DIRS=libs AMS_Test
+
+FLAGS=-mmcu=atmega32u4 -Os -DF_CPU=8000000UL -std=gnu99 -Wall
+LIBS=-Wl,-u,vfprintf -lprintf_flt -lm
+
+main: main.c
+	avr-gcc $@.c libs/*.c $(FLAGS) -I$(DIRS) -L$(DIRS) $(LIBS) -o $@.o
+	avr-objcopy -O ihex $@.o $@.hex 	
+build:
+	avr-gcc $@.c libs/*.c $(FLAGS) -I$(DIRS) -L$(DIRS) $(LIBS) -o $@.o
+	avr-objcopy -O ihex $@.o $@.hex 
+	teensy_loader_cli -mmcu=atmega32u4 -w @.hex
+clean:
+	rm *.o
+	rm *.hex
+
+rebuild: clean all
+
+all: $(TARGETS)
