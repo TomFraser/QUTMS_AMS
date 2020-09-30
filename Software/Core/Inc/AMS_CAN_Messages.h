a71d78b26c411805158a8858ba468db875801a56
TomFraser <tr.fraser@bigpond.com>
2020-09-30 22:58:35 +1000

    Reformmated The CAN_Messages library

--- Software/Core/Inc/AMS_CAN_Messages.h
+++ Software/Core/Inc/AMS_CAN_Messages.h
@@ -9,6 +9,7 @@
 #define INC_AMS_CAN_MESSAGES_H_
 
 #include "stdbool.h"
+#include "main.h"
 
 #define HEARTBEAT_RESPONSE_ID 0x8404001
 #define CELL_VOLTAGE_SHUTDOWN_ID 0x400000
@@ -22,237 +23,120 @@
 #define TRANSMIT_CHARGE_STATE_ID 0x1040C004
 #define READY_ID 0x1040C000
 #define BMS_TRANSMIT_DEVICE_ID_ID 0x1048C000
+#define HEARTBEAT_REQUEST_ID 0x00 // TODO FIX
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[2];
 } AMS_CellVoltageShutdown_t;
-
 // Should the voltage be a .1f?
-AMS_CellVoltageShutdown_t AMS_Compose_CellVoltageShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t voltage)
-{
-	AMS_CellVoltageShutdown_t p;
-	p.extID = CELL_VOLTAGE_SHUTDOWN_ID;
-	p.data[0] = (cellNum & 0xF) << 4 | (bmsID & 0xF);
-	p.data[1] = (voltage & 0xFF);
-
-	return p;
-}
+AMS_CellVoltageShutdown_t AMS_Compose_CellVoltageShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t voltage);
 // Should the voltage be a .1f?
-void AMS_Parse_CellVoltageShutdown(AMS_CellVoltageShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* voltage)
-{
-	*cellNum = (uint8_t)(packet.data[0] >> 4);
-	*bmsID = (uint8_t)(packet.data[0] & 0xF);
-	*voltage = (uint8_t)(packet.data[1] & 0xFF);
-}
+void AMS_Parse_CellVoltageShutdown(AMS_CellVoltageShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* voltage);
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[2];
 } AMS_CellTemperatureShutdown_t;
-
 // Should temperature be a .1f?
-AMS_CellTemperatureShutdown_t AMS_Compose_CellTemperatureShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t temperature)
-{
-	AMS_CellTemperatureShutdown_t p;
-	p.extID = CELL_TEMPERATURE_SHUTDOWN_ID;
-	p.data[0] = (cellNum & 0xF) << 4 | (bmsID & 0xF);
-	p.data[1] = (temperature & 0xFF);
-
-	return p;
-}
+AMS_CellTemperatureShutdown_t AMS_Compose_CellTemperatureShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t temperature);
 // Should temperature be a .1f?
-void AMS_Parse_CellTemperatureShutdown(AMS_CellTemperatureShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* temperature)
-{
-	*cellNum = (uint8_t)(packet.data[0] >> 4);
-	*bmsID = (uint8_t)(packet.data[0] & 0xF);
-	*temperature = (uint8_t)(packet.data[1] & 0xFF);
-}
+void AMS_Parse_CellTemperatureShutdown(AMS_CellTemperatureShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* temperature);
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[2];
 } AMS_MissingBMS_t;
+AMS_MissingBMS_t AMS_Compose_MissingBMS(uint8_t data[12])
+{
+	//TODO
+}
+void AMS_Parse_MissingBMS(uint8_t* data[12])
+{
+	//TODO
+}
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_HeartbeatRequest_t;
+AMS_HeartbeatRequest_t AMS_Compose_HeartbeatRequest()
+{
+	AMS_HeartbeatRequest_t p;
+	p.extID = HEARTBEAT_REQUEST_ID;
+
+	return p;
+}
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[4];
 } AMS_HeartbeatResponse_t;
-
-AMS_HeartbeatResponse_t AMS_Compose_HeartbeatResponse(bool HVAn, bool HVBn, bool precharge, bool HVAp, bool HVBp, uint8_t averageVoltage, uint16_t runtime)
-{
-	AMS_HeartbeatResponse_t p;
-	p.extID = HEARTBEAT_RESPONSE_ID;
-
-	p.data[0] = HVAn | (HVBn << 1) | (precharge << 2) | (HVAp << 4) | (HVBp << 5);
-	p.data[1] = averageVoltage;
-	p.data[2] = (runtime & 0xFF);
-	p.data[3] = (runtime >> 8);
-
-	return p;
-}
-
-void AMS_Parse_HeartbeatResponse(AMS_HeartbeatResponse_t packet, bool* HVAn, bool* HVBn, bool* precharge, bool* HVAp, bool* HVBp, uint8_t* averageVoltage, uint16_t* runtime)
-{
-	*HVAn = (bool)(packet.data[0] & 0x1);
-	*HVBn = (bool)(packet.data[0] & 0x2);
-	*precharge = (bool)(packet.data[0] & 0x4);
-
-	*HVAp = (bool)(packet.data[0] & 0x10);
-	*HVBp = (bool)(packet.data[0] & 0x20);
-
-	*averageVoltage = (uint8_t)(packet.data[1]);
-	*runtime = (uint16_t)(packet.data[3] << 8 | packet.data[2]);
-}
+AMS_HeartbeatResponse_t AMS_Compose_HeartbeatResponse(bool HVAn, bool HVBn, bool precharge, bool HVAp, bool HVBp, uint8_t averageVoltage, uint16_t runtime);
+void AMS_Parse_HeartbeatResponse(AMS_HeartbeatResponse_t packet, bool* HVAn, bool* HVBn, bool* precharge, bool* HVAp, bool* HVBp, uint8_t* averageVoltage, uint16_t* runtime);
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_StartUp_t;
-
-AMS_StartUp_t AMS_Compose_StartUp()
-{
-	AMS_StartUp_t p;
-	p.extID = START_UP_ID;
-
-	return p;
-}
+AMS_StartUp_t AMS_Compose_StartUp();
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_ResetTractive_t;
-
-AMS_ResetTractive_t AMS_Compose_ResetTractive()
-{
-	AMS_ResetTractive_t p;
-	p.extID = RESET_TRACTIVE_ID;
-
-	return p;
-}
+AMS_ResetTractive_t AMS_Compose_ResetTractive();
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_Shutdown_t;
-
-AMS_Shutdown_t AMS_Compose_Shutdown()
-{
-	AMS_Shutdown_t p;
-	p.extID = SHUTDOWN_ID;
-
-	return p;
-}
+AMS_Shutdown_t AMS_Compose_Shutdown();
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_RequestTemperature_t;
-
-AMS_RequestTemperature_t AMS_Compose_RequestTemperature()
-{
-	AMS_RequestTemperature_t p;
-	p.extID = REQUEST_TEMPERATURE_ID;
-
-	return p;
-}
+AMS_RequestTemperature_t AMS_Compose_RequestTemperature();
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[1];
 } AMS_TransmitTemperature_t;
-
-AMS_TransmitTemperature_t AMS_Compose_TransmitTemperature(uint8_t temperature)
-{
-	AMS_TransmitTemperature_t p;
-	p.extID = TRANSMIT_TEMPERATURE_ID;
-	p.data[0] = temperature;
-
-	return p;
-}
-
-void AMS_Parse_TransmitTemperature(AMS_TransmitTemperature_t packet, uint8_t* temperature)
-{
-	*temperature = packet.data[0];
-}
+AMS_TransmitTemperature_t AMS_Compose_TransmitTemperature(uint8_t temperature);
+void AMS_Parse_TransmitTemperature(AMS_TransmitTemperature_t packet, uint8_t* temperature);
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_RequestChargeState_t;
-
-AMS_RequestChargeState_t AMS_Compose_RequestChargeState()
-{
-	AMS_RequestChargeState_t p;
-	p.extID = REQUEST_CHARGE_STATE_ID;
-
-	return p;
-}
+AMS_RequestChargeState_t AMS_Compose_RequestChargeState();
 
 typedef struct
 {
 	uint32_t extID;
 	uint8_t data[2];
 } AMS_TransmitChargeState_t;
-
-AMS_TransmitChargeState_t AMS_Compose_TransmitChargeState(uint16_t chargeState)
-{
-	AMS_TransmitChargeState_t p;
-	p.extID = TRANSMIT_CHARGE_STATE_ID;
-	p.data[0] = chargeState >> 8;
-	p.data[1] = (chargeState & 0xFF);
-
-	return p;
-}
-
-void AMS_Parse_TransmitChargeState(AMS_TransmitChargeState_t packet, uint16_t* chargeState)
-{
-	*chargeState = (packet.data[0] << 8 | packet.data[1]);
-}
+AMS_TransmitChargeState_t AMS_Compose_TransmitChargeState(uint16_t chargeState);
+void AMS_Parse_TransmitChargeState(AMS_TransmitChargeState_t packet, uint16_t* chargeState);
 
 typedef struct
 {
 	uint32_t extID;
 } AMS_Ready_t;
-
-AMS_Ready_t AMS_Construct_Ready() {
-	AMS_Ready_t p;
-	p.extID = READY_ID;
-
-	return p;
-}
+AMS_Ready_t AMS_Construct_Ready();
 
 typedef struct // Spec could change for this packet.
 {
 	uint32_t extID;
 	uint8_t data[4];
 } BMS_TransmitDeviceID_t;
-
-BMS_TransmitDeviceID_t BMS_Compose_TransmitDeviceID(uint32_t deviceID)
-{
-	BMS_TransmitDeviceID_t p;
-	p.extID = BMS_TRANSMIT_DEVICE_ID_ID;
-	p.data[0] = (deviceID & 0xFF);
-	p.data[1] = (deviceID >> 8) & 0xFF;
-	p.data[2] = (deviceID >> 16) & 0xFF;
-	p.data[3] = (deviceID >> 24) & 0xFF;
-
-	return p;
-}
-
-void BMS_Parse_TransmitDeviceID(BMS_TransmitDeviceID_t packet, uint32_t* deviceID)
-{
-	*deviceID = (packet.data[3] << 24 | packet.data[2] << 16 | packet.data[1] << 8 | packet.data[0]);
-}
+BMS_TransmitDeviceID_t BMS_Compose_TransmitDeviceID(uint32_t deviceID);
+void BMS_Parse_TransmitDeviceID(BMS_TransmitDeviceID_t packet, uint32_t* deviceID);
 
 #endif /* INC_AMS_CAN_MESSAGES_H_ */
---
+++ Software/Core/Src/AMS_CAN_Messages.c
@@ -0,0 +1,175 @@
+/*
+ * AMS_CAN_Messages.c
+ *
+ *  Created on: 30 Sep 2020
+ *      Author: Tom
+ */
+
+
+#include "AMS_CAN_Messages.h"
+
+/* Cell Voltage Shutdown */
+AMS_CellVoltageShutdown_t AMS_Compose_CellVoltageShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t voltage)
+{
+	AMS_CellVoltageShutdown_t p;
+	p.extID = CELL_VOLTAGE_SHUTDOWN_ID;
+	p.data[0] = (cellNum & 0xF) << 4 | (bmsID & 0xF);
+	p.data[1] = (voltage & 0xFF);
+
+	return p;
+}
+
+void AMS_Parse_CellVoltageShutdown(AMS_CellVoltageShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* voltage)
+{
+	*cellNum = (uint8_t)(packet.data[0] >> 4);
+	*bmsID = (uint8_t)(packet.data[0] & 0xF);
+	*voltage = (uint8_t)(packet.data[1] & 0xFF);
+}
+
+/* Cell Temperature Shutdown */
+AMS_CellTemperatureShutdown_t AMS_Compose_CellTemperatureShutdown(uint8_t cellNum, uint8_t bmsID, uint8_t temperature)
+{
+	AMS_CellTemperatureShutdown_t p;
+	p.extID = CELL_TEMPERATURE_SHUTDOWN_ID;
+	p.data[0] = (cellNum & 0xF) << 4 | (bmsID & 0xF);
+	p.data[1] = (temperature & 0xFF);
+
+	return p;
+}
+
+void AMS_Parse_CellTemperatureShutdown(AMS_CellTemperatureShutdown_t packet, uint8_t* cellNum, uint8_t* bmsID, uint8_t* temperature)
+{
+	*cellNum = (uint8_t)(packet.data[0] >> 4);
+	*bmsID = (uint8_t)(packet.data[0] & 0xF);
+	*temperature = (uint8_t)(packet.data[1] & 0xFF);
+}
+
+/* Heartbeat Response */
+AMS_HeartbeatResponse_t AMS_Compose_HeartbeatResponse(bool HVAn, bool HVBn, bool precharge, bool HVAp, bool HVBp, uint8_t averageVoltage, uint16_t runtime)
+{
+	AMS_HeartbeatResponse_t p;
+	p.extID = HEARTBEAT_RESPONSE_ID;
+
+	p.data[0] = HVAn | (HVBn << 1) | (precharge << 2) | (HVAp << 4) | (HVBp << 5);
+	p.data[1] = averageVoltage;
+	p.data[2] = (runtime & 0xFF);
+	p.data[3] = (runtime >> 8);
+
+	return p;
+}
+
+void AMS_Parse_HeartbeatResponse(AMS_HeartbeatResponse_t packet, bool* HVAn, bool* HVBn, bool* precharge, bool* HVAp, bool* HVBp, uint8_t* averageVoltage, uint16_t* runtime)
+{
+	*HVAn = (bool)(packet.data[0] & 0x1);
+	*HVBn = (bool)(packet.data[0] & 0x2);
+	*precharge = (bool)(packet.data[0] & 0x4);
+
+	*HVAp = (bool)(packet.data[0] & 0x10);
+	*HVBp = (bool)(packet.data[0] & 0x20);
+
+	*averageVoltage = (uint8_t)(packet.data[1]);
+	*runtime = (uint16_t)(packet.data[3] << 8 | packet.data[2]);
+}
+
+/* Start Up */
+AMS_StartUp_t AMS_Compose_StartUp()
+{
+	AMS_StartUp_t p;
+	p.extID = START_UP_ID;
+
+	return p;
+}
+
+/* Reset Tractive */
+AMS_ResetTractive_t AMS_Compose_ResetTractive()
+{
+	AMS_ResetTractive_t p;
+	p.extID = RESET_TRACTIVE_ID;
+
+	return p;
+}
+
+/* Shutdown */
+AMS_Shutdown_t AMS_Compose_Shutdown()
+{
+	AMS_Shutdown_t p;
+	p.extID = SHUTDOWN_ID;
+
+	return p;
+}
+
+/* Request Temperature */
+AMS_RequestTemperature_t AMS_Compose_RequestTemperature()
+{
+	AMS_RequestTemperature_t p;
+	p.extID = REQUEST_TEMPERATURE_ID;
+
+	return p;
+}
+
+/* Transmit Temperature */
+AMS_TransmitTemperature_t AMS_Compose_TransmitTemperature(uint8_t temperature)
+{
+	AMS_TransmitTemperature_t p;
+	p.extID = TRANSMIT_TEMPERATURE_ID;
+	p.data[0] = temperature;
+
+	return p;
+}
+
+void AMS_Parse_TransmitTemperature(AMS_TransmitTemperature_t packet, uint8_t* temperature)
+{
+	*temperature = packet.data[0];
+}
+
+/* Request Charge State */
+AMS_RequestChargeState_t AMS_Compose_RequestChargeState()
+{
+	AMS_RequestChargeState_t p;
+	p.extID = REQUEST_CHARGE_STATE_ID;
+
+	return p;
+}
+
+/* Transmit Charge State */
+AMS_TransmitChargeState_t AMS_Compose_TransmitChargeState(uint16_t chargeState)
+{
+	AMS_TransmitChargeState_t p;
+	p.extID = TRANSMIT_CHARGE_STATE_ID;
+	p.data[0] = chargeState >> 8;
+	p.data[1] = (chargeState & 0xFF);
+
+	return p;
+}
+
+void AMS_Parse_TransmitChargeState(AMS_TransmitChargeState_t packet, uint16_t* chargeState)
+{
+	*chargeState = (packet.data[0] << 8 | packet.data[1]);
+}
+
+/* Ready */
+AMS_Ready_t AMS_Construct_Ready()
+{
+	AMS_Ready_t p;
+	p.extID = READY_ID;
+
+	return p;
+}
+/* Transmit Device ID */
+BMS_TransmitDeviceID_t BMS_Compose_TransmitDeviceID(uint32_t deviceID)
+{
+	BMS_TransmitDeviceID_t p;
+	p.extID = BMS_TRANSMIT_DEVICE_ID_ID;
+	p.data[0] = (deviceID & 0xFF);
+	p.data[1] = (deviceID >> 8) & 0xFF;
+	p.data[2] = (deviceID >> 16) & 0xFF;
+	p.data[3] = (deviceID >> 24) & 0xFF;
+
+	return p;
+}
+
+void BMS_Parse_TransmitDeviceID(BMS_TransmitDeviceID_t packet, uint32_t* deviceID)
+{
+	*deviceID = (packet.data[3] << 24 | packet.data[2] << 16 | packet.data[1] << 8 | packet.data[0]);
+}
+
